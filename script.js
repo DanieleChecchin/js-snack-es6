ec5bafc329fb54a9209c6247083641846268cb82
DanieleChecchin <danielechecchin26@gmail.com>
2024-08-30 15:58:54 +0200

    ticket

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const guests = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'G
 ];
 
 const list = guests.map((guestName, i) => ({
-    tableName: tableName,
+    tableName: tableName, // se sono uguali basta mettere la chiave 'il primo'
     guestName: guestName,
     place: i + 1
 }))
@@ -37,5 +37,47 @@ console.log(over70grades);
 
 // 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
 
-const over70and120 = students.filter(student => student.grades > 70 && student.id > 120);
+const over70and120 = over70grades.filter(student => student.id > 120);
 console.log(over70and120);
+
+// SNACK 3
+
+/**
+Creare un array di oggetti: -V-
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. -V-
+Stampare in console la bici con peso minore utilizzando destructuring e template literal
+ */
+
+const bikes = [
+    { name: 'Bianchi', weight: 11 },
+    { name: 'Atala', weight: 7 },
+    { name: 'Olmo', weight: 10 },
+    { name: 'Casati', weight: 6 },
+    { name: 'Colnago', weight: 8 }
+];
+
+bikes.sort(function (a, b) {
+    return a.weight - b.weight;
+});
+
+console.log(bikes[0]);
+
+let { name, weight } = bikes;
+console.log(` `)
+
+
+
+
+
+
+//console.log(`La bicicletta più leggera è la ${name} e pesa ${weight} kg`)
+
+// SNACK 4
+
+/**
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+ */
